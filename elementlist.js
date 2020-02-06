e848b6dd3cdf93f20f382dbad74d1f546d55c9cf
Travis <travis@travis-ci.org>
2020-02-06 14:04:34 +0000

    Docs updated by Travis

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","splitbrain\\PHPArchive\\Archive"],["c","splitbrain\\PHPArchive\\ArchiveCorruptedException"],["c","splitbrain\\PHPArchive\\ArchiveIllegalCompressionException"],["c","splitbrain\\PHPArchive\\ArchiveIOException"],["c","splitbrain\\PHPArchive\\FileInfo"],["c","splitbrain\\PHPArchive\\FileInfoException"],["c","splitbrain\\PHPArchive\\Tar"],["c","splitbrain\\PHPArchive\\Zip"]];
